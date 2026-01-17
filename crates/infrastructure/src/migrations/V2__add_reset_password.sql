83559e6ad19194e96193fec1e4e70e84cdd2b493
mystique09 <knightkrypto7@gmail.com>
2026-01-17 15:24:58 +0800

    chore: use uuid in db tables add: reset_passwords table

---
+++ crates/infrastructure/src/migrations/V2__add_reset_password.sql
@@ -0,0 +1,12 @@
+CREATE TABLE
+    "reset_passwords" (
+        id UUID NOT NULL PRIMARY KEY,
+        user_id UUID NOT NULL,
+        token TEXT NOT NULL,
+        consumed BOOLEAN DEFAULT FALSE,
+        created_at TIMESTAMPTZ NOT NULL DEFAULT NOW (),
+        updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW (),
+        CONSTRAINT reset_passwords_token_unique UNIQUE (token)
+    );
+
+ALTER TABLE "reset_passwords" ADD CONSTRAINT "fk-reset_passwords-user_id" FOREIGN KEY ("user_id") REFERENCES "users" ("id") ON DELETE CASCADE;
